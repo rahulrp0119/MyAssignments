f725eb9d9a42fa6286a8ab2a5d9b54d103871c01
rahulrp0119 <167859168+rahulrp0119@users.noreply.github.com>
2024-04-23 20:02:31 +0530

    Add files via upload
    day1

---
+++ day1/SampleCode.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class SampleCode {
+
+	//main-ctrl+space
+	public static void main(String[] args) {
+		//Syso-ctrl+space
+		System.out.println("Hello World...!");
+	}
+}
